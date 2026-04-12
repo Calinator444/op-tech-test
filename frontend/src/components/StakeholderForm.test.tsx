28be011c7ac2f2a68f6a3a16155b22622ec1ac56
Calinator444 <calebwilliams@ssw.com.au>
2026-04-12 14:43:00 +1000

    added test to ensure that stakeholder form displays message when filled in successfully

--- frontend/src/components/StakeholderForm.test.tsx
+++ frontend/src/components/StakeholderForm.test.tsx
@@ -1,16 +1,24 @@
-import { describe, expect, it } from "vitest";
+import { afterAll, describe, expect, it, vi } from "vitest";
 import StakeholderForm from "./StakeholderForm";
 import { MemoryRouter } from "react-router-dom";
-import { render, waitFor } from "@testing-library/react";
+import { render, waitFor, queryByAttribute } from "@testing-library/react";
 import { screen } from "@testing-library/react";
+import { act } from "react";
+import {toast } from 'react-toastify';
+import { wait } from "@testing-library/user-event/dist/cjs/utils/index.js";
+import * as StakeholderService from "@/services/stakeholderService";
+import { getEmailExists } from "@/services/stakeholderService";
+import userEvent from '@testing-library/user-event';                                                    
 
-describe('StakeholderForm', () => {
+
+describe('StakeholerForm', () => {
     it('renders form fields correctly', () => {
-        render(
+        const result = render(
             <MemoryRouter>
                 <StakeholderForm />
             </MemoryRouter>
             );
+        
         expect(screen.getByLabelText('Title')).toBeInTheDocument();
         expect(screen.getByLabelText('First Name')).toBeInTheDocument();
         expect(screen.getByLabelText('Last Name')).toBeInTheDocument();
@@ -18,7 +26,6 @@ describe('StakeholderForm', () => {
         expect(screen.getByLabelText('Role')).toBeInTheDocument();
         expect(screen.getByLabelText('Organisation')).toBeInTheDocument();
     });
-
 
     it('shows validation errors for required fields', async ()=>{
         render(
@@ -34,6 +41,31 @@ describe('StakeholderForm', () => {
             expect(screen.getByText("Role is required")).toBeInTheDocument();
             expect(screen.getByText("Organisation is required")).toBeInTheDocument();
         });
-        
     })
+    it('displays a success toast on successful submission', async () => {
+
+        const user = userEvent.setup();
+        vi.spyOn(StakeholderService, 'getEmailExists').mockImplementation(() => {
+            return Promise.resolve(false);
+        });
+        vi.spyOn(StakeholderService, 'createStakeholder').mockImplementation(() => {
+            return Promise.resolve(undefined);
+        });
+        const toastSpy = vi.spyOn(toast, 'success').mockImplementation(() => "id");
+        render(
+        <MemoryRouter>
+            <StakeholderForm />
+        </MemoryRouter>);
+        await user.type(screen.getByLabelText('First Name'), 'Test');                                       
+        await user.type(screen.getByLabelText('Last Name'), 'User');                                      
+        await user.type(screen.getByLabelText('Email'), 'alice@example.com');
+        await user.type(screen.getByLabelText('Role'), 'Tester');                                           
+        await user.type(screen.getByLabelText('Organisation'), 'Testing Inc');
+        const submitButton = screen.getByRole('button');
+        submitButton.click();        
+        await waitFor(() => {
+            expect(toastSpy).toHaveBeenCalledWith("Stakeholder created successfully!");
+        });
+
+    });
 });
