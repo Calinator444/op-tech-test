dde980a1532205526517619cf9f68f8d9747c989
Calinator444 <calebwilliams@ssw.com.au>
2026-04-12 16:31:41 +1000

    forwarded vanilla html props to pagination components

--- frontend/src/components/Pagination.tsx
+++ frontend/src/components/Pagination.tsx
@@ -2,25 +2,20 @@ import React from 'react';
 import clsx from 'clsx';
 import { MdKeyboardDoubleArrowLeft, MdKeyboardDoubleArrowRight, MdOutlineKeyboardArrowLeft, MdOutlineKeyboardArrowRight } from 'react-icons/md';
 
-interface PaginationContainerProps {
-  children: React.ReactNode;
+export function PaginationContainer({ children, className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  return <div className={clsx('pagination-container', className)} {...props}>{children}</div>;
 }
 
-export function PaginationContainer({ children }: PaginationContainerProps) {
-  return <div className="pagination-container">{children}</div>;
-}
-
-interface PaginationSelectorProps {
+interface PaginationSelectorProps extends React.SelectHTMLAttributes<HTMLSelectElement> {
   value: number;
   options: number[];
-  onChange: (value: number) => void;
 }
 
-export function PaginationSelector({ value, options, onChange }: PaginationSelectorProps) {
+export function PaginationSelector({ value, options, className, ...props }: PaginationSelectorProps) {
   return (
     <div className="pagination-selector">
       Page Size:
-      <select className="select" value={value} onChange={(e) => onChange(Number(e.target.value))}>
+      <select className={clsx('select', className)} value={value} {...props}>
         {options.map((size, index) => (
           <option key={index} value={size}>
             {size}
@@ -31,69 +26,56 @@ export function PaginationSelector({ value, options, onChange }: PaginationSelec
   );
 }
 
-interface PaginationGroupProps {
-  children: React.ReactNode;
-}
-
-export function PaginationGroup({ children }: PaginationGroupProps) {
-  return <div className="pagination">{children}</div>;
+export function PaginationGroup({ children, className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  return <div className={clsx('pagination', className)} {...props}>{children}</div>;
 }
 
 type PaginationVariant = 'selected' | 'ghost';
 
-interface PaginationButtonProps {
-  onClick: () => void;
-  disabled?: boolean;
-  className?: string;
+interface PaginationButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: PaginationVariant;
-  children: React.ReactNode;
 }
 
-export function PaginationButton({ onClick, className, disabled, variant = 'ghost', children }: PaginationButtonProps) {
-
+export function PaginationButton({ variant = 'ghost', className, children, ...props }: PaginationButtonProps) {
   return (
     <button
       className={clsx('pagination-button', variant, className)}
-      onClick={onClick}
-      disabled={disabled}
+      {...props}
     >
       {children}
     </button>
   );
 }
 
-interface PaginationNavProps {
-  onClick: () => void;
-  disabled?: boolean;
-}
+type PaginationNavProps = Omit<PaginationButtonProps, 'variant' | 'children'>;
 
-export function PaginationEnd({ onClick, disabled }: PaginationNavProps) {
+export function PaginationEnd({ className, ...props }: PaginationNavProps) {
   return (
-    <PaginationButton onClick={onClick} disabled={disabled} className="end">
+    <PaginationButton className={clsx('end', className)} {...props}>
       <MdKeyboardDoubleArrowRight />
     </PaginationButton>
   );
 }
 
-export function PaginationStart({ onClick, disabled }: PaginationNavProps) {
+export function PaginationStart({ className, ...props }: PaginationNavProps) {
   return (
-    <PaginationButton onClick={onClick} disabled={disabled} className="end">
+    <PaginationButton className={clsx('end', className)} {...props}>
       <MdKeyboardDoubleArrowLeft />
     </PaginationButton>
   );
 }
 
-export function PaginationNext({ onClick, disabled }: PaginationNavProps) {
+export function PaginationNext({ className, ...props }: PaginationNavProps) {
   return (
-    <PaginationButton onClick={onClick} disabled={disabled} className="end">
+    <PaginationButton className={clsx('end', className)} {...props}>
       <MdOutlineKeyboardArrowRight />
     </PaginationButton>
   );
 }
 
-export function PaginationPrevious({ onClick, disabled }: PaginationNavProps) {
+export function PaginationPrevious({ className, ...props }: PaginationNavProps) {
   return (
-    <PaginationButton onClick={onClick} disabled={disabled} className="end">
+    <PaginationButton className={clsx('end', className)} {...props}>
       <MdOutlineKeyboardArrowLeft />
     </PaginationButton>
   );
